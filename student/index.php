2577de33524d461096e14e4749caf9ba157c8fb2
kaxim <itsmekazim@gmail.com>
2017-05-15 07:59:34 +0530

    Update index.php

--- student/index.php
+++ student/index.php
@@ -1,1 +1,32 @@
-Hello.txt
+<?php
+	session_start();
+		if(isset($_SESSION['login_user'])){
+		echo '<script language="javascript">';
+		echo 'alert("message login_user 1")';
+		echo '</script>';
+		if($_SESSION['login_user'] != 0){
+			echo '<script language="javascript">';
+			echo 'alert("message login_user sent")';
+			echo '</script>';
+			session_destroy();
+			$_SESSION['login_user']= ''; // Devalidating Session
+			header("location: /HFM");
+		}
+	}else{
+		session_destroy();
+		header("location: /HFM");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/dashboard.php">
+<title>SB Admin 2</title>
+<script language="javascript">
+    window.location.href = "pages/dashboard.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/dashboard.php">/pages/dashboard.php</a>
+</body>
+</html>
